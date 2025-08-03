fdfe34c17b4ee072b734dd7da879700358bc1bd7
NiGHTS <your_email@example.com>
2025-08-03 09:02:42 +0900

    Auto commit from script

--- push.sh
+++ push.sh
@@ -4,13 +4,17 @@
 git status
 
 # ステージング
-git add .
+if [ -z "$1" ]; then
+    git add .
+else
+    git add "$1"
+fi
 
 # コミット（引数がない場合はデフォルトメッセージ）
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
     git commit -m "Auto commit from script"
 else
-    git commit -m "$1"
+    git commit -m "$2"
 fi
 
 # プッシュ
